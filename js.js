a18ed2ea04a5859a0f17941aec4b69d05c26cbdf
Tbruno15 <92650822+Tbruno15@users.noreply.github.com>
2021-10-16 17:54:58 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,3 @@
+<script type='text/javascript'>$(function(){$(window).scroll(function(){if($(this).scrollTop()!=0){$("#rb-top")
+.fadeIn()}else{$("#rb-top").fadeOut()}});$("#rb-top").click(function(){$("body,html").animate({scrollTop:0},800);return false})});
+</script>
